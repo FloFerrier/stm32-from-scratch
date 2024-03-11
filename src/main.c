5c0522e019af8e9d381db2ece5b72474bf65d5fc
ferrierf <florian.ferrier@aol.fr>
2024-03-11 17:12:13 +0100

    Add SysTick handler to have a 1 ms counter

--- src/main.c
+++ src/main.c
@@ -9,6 +9,13 @@
 #include "mock_delay.h"
 #endif
 
+#define STK_CTRL (*(volatile uint32_t *)0xE000E010)
+#define STK_RELOAD (*(volatile uint32_t *)0xE000E014)
+#define STK_VAL (*(volatile uint32_t *)0xE000E018)
+#define STK_CALIB (*(volatile uint32_t *)0xE000E01C)
+
+extern volatile uint32_t systick_counter;
+
 /* Blink User Led on Nucleo-f446re board
  * User Led = GPIOA5
  */
@@ -16,9 +23,13 @@ int MAIN(void) {
 
 	/* After reset, the CPU clock frequency is 16MHz */
 
+    STK_CTRL |= 0x00000007;
+    STK_RELOAD = 16000u; // each 1 ms
+
 	userLed_setup();
 
 	LOOP {
+        printf("SysTicks: %lu\r\n", systick_counter);
 		userLed_set(USER_LED_STATE_OFF);
 		delay_ms(500u);
         userLed_set(USER_LED_STATE_ON);
